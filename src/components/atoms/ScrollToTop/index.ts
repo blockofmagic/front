f6752f65129dde92d08292d4b3d6be67a7afb3df
Pinterid <david.pinterics@gmail.com>
2020-10-09 09:48:23 +0200

    Add ScrollToTop component
    The component for scrolling to the top of the page was added.

---
+++ src/components/atoms/ScrollToTop/index.ts
@@ -0,0 +1,47 @@
+//#region > Imports
+//> React
+// Contains all the functionality necessary to define React components
+import React from "react";
+// DOM bindings for React Router
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import { History, LocationState } from "history";
+//#endregion
+
+//#region > Interfaces
+interface Props extends RouteComponentProps {}
+//#endregion
+
+//#region > Components
+/**
+ * @class ScrollToTop Component
+ * @description When reloading or switching a page, ReactJS now starts at the
+ *              top of the page, and no longer on the last position.
+ */
+class ScrollToTop extends React.Component<Props> {
+  componentDidUpdate(prevProps: Props) {
+    const { history } = this.props;
+    const { location } = history;
+
+    if (location !== prevProps.location) {
+      // Scroll to top of viewport
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+//#endregion
+
+//#region > Exports
+//> Default Component
+// Got access to the history object’s properties and the closest
+// <Route>'s match.
+export default withRouter(ScrollToTop);
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019-2020 Nico Schett
+ */
