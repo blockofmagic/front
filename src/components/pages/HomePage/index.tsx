3b810d3c9fd12fe0bfb0913f8227819f623aa4e3
Pinterid <david.pinterics@gmail.com>
2020-10-22 22:25:21 +0200

    Update homepage
    The action card was added to the homepage.

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -2,15 +2,40 @@
 //> React
 // Contains all the functionality necessary to define React components
 import React from "react";
+import { Grid, Row, Col } from "react-bootstrap";
+//> Components
+import { UserActionCard } from "../../molecules";
+//#endregion
+
+//#region > Interfaces
+interface State {
+  actionCard: any;
+}
+interface Location {
+  state: State;
+}
+interface Props {
+  location: Location;
+}
 //#endregion
 
 //#region > Components
 /** @class This component displays the landing page including login and register */
-class HomePage extends React.Component {
+class HomePage extends React.Component<Props> {
   render() {
+    const activeActionCard = this.props.location?.state?.actionCard;
+
     return (
       <div id="home" className="pt-5">
-        TEST
+        <Grid>
+          <Row>
+            <Col md={4}>
+              <UserActionCard
+                activeIndex={activeActionCard ? activeActionCard : 0}
+              />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
