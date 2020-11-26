65314c618531a8202f111bab803c8eabb13c7d69
Dominik Egger <egger.dominik1@gmail.com>
2020-11-26 16:25:03 +0100

    Add components
    The components for the homepage were added

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -89,7 +89,172 @@ class HomePage extends React.Component {
   render() {
     return (
       <div id="home" className="pt-5">
-        TEST
+        <MDBContainer>
+          <h1 className="text-center font-weight-bold text-justify headline" >BlockOfMagic</h1>
+          <h2 className="text-center text-justify subheadline" >verhindert Leid auf der Welt</h2>
+        </MDBContainer>
+        <br></br>
+        <br></br>
+        <br></br>
+        <MDBContainer>
+          <div id="row1">
+            <div className="button-container">
+              <MDBBtn className="button-left">Transactions</MDBBtn>
+              <MDBBtn className="button-centre">Log in / My account</MDBBtn>
+              <MDBBtn className="button-right">Statistics and graphics</MDBBtn>
+            </div>
+          </div>
+        </MDBContainer>
+        <br></br>
+        <br></br>
+        <br></br>
+        <MDBContainer>
+          <div className="d-flex justify-content-center div-background-color">
+            <MDBBadge className="text-center div-background-color" ><h3>Was ist BlockOfMagic</h3></MDBBadge>
+          </div>
+          <br></br>
+          <p className="text-center text-justify" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </MDBContainer>
+        <br></br>
+        <br></br>
+        <br></br>
+        <MDBContainer>
+        <MDBNav className="nav-tabs mt-5 justify-content-center nav-background-color">
+
+          <MDBNavItem className="nav-link">
+            <MDBNavLink link to="#" active={this.state.activeItem === "1"} onClick={this.toggle("1")} role="tab" >
+              Über uns
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem className="nav-link">
+            <MDBNavLink link to="#" active={this.state.activeItem === "2"} onClick={this.toggle("2")} role="tab" >
+              Technik
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem className="nav-link">
+            <MDBNavLink link to="#" active={this.state.activeItem === "3"} onClick={this.toggle("3")} role="tab" >
+              Team
+            </MDBNavLink>
+          </MDBNavItem>
+        </MDBNav>
+        <MDBTabContent activeItem={this.state.activeItem} >
+          <MDBTabPane tabId="1" role="tabpanel">
+            <h3 className="mt-5">Example Headline 3</h3>
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Nihil odit magnam minima, soluta doloribus reiciendis
+              molestiae placeat unde eos molestias. Quisquam aperiam,
+              pariatur. Tempora, placeat ratione porro voluptate odit
+              minima.
+            </p>
+            <h4 className="mt-5">Example Headline 4</h4>
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Nihil odit magnam minima, soluta doloribus reiciendis
+              molestiae placeat unde eos molestias. Quisquam aperiam,
+              pariatur. Tempora, placeat ratione porro voluptate odit
+              minima.
+            </p>
+          </MDBTabPane>
+          <MDBTabPane tabId="2" role="tabpanel">
+            <h3 className="mt-5">Example Headline 3</h3>
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Nihil odit magnam minima, soluta doloribus reiciendis
+              molestiae placeat unde eos molestias. Quisquam aperiam,
+              pariatur. Tempora, placeat ratione porro voluptate odit
+              minima.
+            </p>
+            <h4 className="mt-5">Example Headline 4</h4>
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Nihil odit magnam minima, soluta doloribus reiciendis
+              molestiae placeat unde eos molestias. Quisquam aperiam,
+              pariatur. Tempora, placeat ratione porro voluptate odit
+              minima.
+            </p>
+          </MDBTabPane>
+          <MDBTabPane tabId="3" role="tabpanel">
+            <h3 className="mt-5">Example Headline 3</h3>
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Nihil odit magnam minima, soluta doloribus reiciendis
+              molestiae placeat unde eos molestias. Quisquam aperiam,
+              pariatur. Tempora, placeat ratione porro voluptate odit
+              minima.
+            </p>
+            <h4 className="mt-5">Line chart</h4>
+            <MDBContainer className="container-blockss">
+            <div className="justify-content-center div-blockss">
+              <div className="justify-content-left div-float-1">
+                <h4 className="teammember-headline text-center">Line chart</h4>
+                <img src="https://avatars0.githubusercontent.com/u/55870326?s=200&v=4" className="rounded mx-auto d-block" alt="thumbnail" />
+              </div>
+              <div className="div-float-2 text-padding">   
+                <p className="text-center text-justify" >Lorem ipsum dolor sitt labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+              </div>
+            </div>
+            </MDBContainer>
+            <br></br>
+            <MDBContainer className="container-blockss">
+            <div className="justify-content-center div-blockss">
+              <div className="justify-content-left div-float-2">
+                <h4 className="teammember-headline text-center">Line chart</h4>
+                <img src="https://avatars0.githubusercontent.com/u/55870326?s=200&v=4" className="rounded mx-auto d-block" alt="thumbnail" />
+              </div>
+              <div className="div-float-1 text-padding">   
+                <p className="text-center text-justify" >Lorem ipsum dolor sitt labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+              </div>
+            </div>
+            </MDBContainer>
+          </MDBTabPane>
+        </MDBTabContent>
+      </MDBContainer>
+      <br></br>
+      <br></br>
+      <br></br>
+      <MDBContainer>
+        <div className="d-flex justify-content-center div-background-color">
+          <MDBBadge className="text-center div-background-color"><h3 className="text-center" >Partner von BlockOfMagic</h3></MDBBadge>
+        </div>   
+        <br></br>        
+        <p className="text-center text-justify" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <br></br> 
+        <img src="https://avatars0.githubusercontent.com/u/55870326?s=200&v=4" className="rounded mx-auto d-block" alt="thumbnail" />
+      </MDBContainer>
+      <br></br>
+      <br></br>
+      <br></br>
+      <MDBContainer>
+        <div className="d-flex justify-content-center div-background-color">
+          <MDBBadge className="text-center div-background-color"><h3>Statistiken und Grafiken vlt.</h3></MDBBadge>
+        </div>       
+      </MDBContainer>
+      <MDBContainer>
+        <h3 className="mt-5">Line chart</h3>
+        <div className="justify-content-center div-blockss">
+          <div className="justify-content-left div-float-1">
+            <Line data={this.state_1.dataLine}/>
+          </div>
+          <div className="float-right div-float-2">
+            <p className="text-center text-justify" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </MDBContainer>  
+      <br></br>
+      <br></br>
+      <br></br>
+      <MDBContainer>
+        <h3 className="mt-5">Line chart</h3>
+        <div className="justify-content-center">
+          <div className="justify-content-left div-float-2">
+            <Line data={this.state_1.dataLine}/>
+          </div>
+          <div className="div-float-1">
+            <p className="text-center text-justify" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </MDBContainer>     
       </div>
     );
   }
