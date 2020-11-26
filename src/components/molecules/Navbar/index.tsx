6d0a4dedd556f3fd660f7506d0551824d30cb05d
Pinterid <david.pinterics@gmail.com>
2020-11-26 21:57:41 +0100

    Update navbar
    The navbar was updated.

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -15,8 +15,20 @@ import {
   MDBContainer,
   MDBSmoothScroll,
   MDBBtn,
+  MDBIcon,
+  MDBNavItem,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
 } from "mdbreact";
+//> Redux
+// Allows to React components read data from a Redux store, and dispatch actions
+// to the store to update data.
+import { connect } from "react-redux";
 
+//> Actions
+// Functions to send data from the application to the store
+import { logoutAction } from "../../../store/actions/userActions";
 //> Style sheet
 import "./navbar.scss";
 //> Images
@@ -24,7 +36,10 @@ import Logo from "../../../assets/navigation/Logo.png";
 //#endregion
 
 //#region > Interfaces
-interface Props extends RouteComponentProps {}
+interface Props extends RouteComponentProps {
+  loggedUser: any;
+  logout: any;
+}
 //#endregion
 
 //#region > Components
@@ -41,7 +56,7 @@ class Navbar extends React.Component<Props> {
   };
 
   render() {
-    const { history } = this.props;
+    const { history, loggedUser } = this.props;
     const { location } = history;
 
     return (
@@ -75,18 +90,50 @@ class Navbar extends React.Component<Props> {
           <MDBNavbarToggler onClick={this.toggleCollapse} />
           <MDBCollapse id="navbarCollapse" isOpen={this.state.isOpen} navbar>
             <MDBNavbarNav right>
-              <Link
-                to={{
-                  pathname: "/",
-                  state: {
-                    actionCard: 1,
-                  },
-                }}
-              >
-                <MDBBtn color="green" outline size="sm">
-                  Sign In
-                </MDBBtn>
-              </Link>
+              {loggedUser.anonymous === false ? (
+                <>
+                  <MDBNavItem>
+                    <MDBDropdown>
+                      <MDBDropdownToggle nav caret>
+                        <img
+                          src="https://avatars1.githubusercontent.com/u/55298934?s=460&u=37517fbfe6f4bc3aa2b1f92fa58a348bd977abbe&v=4"
+                          className="z-depth-0"
+                          alt={loggedUser.username}
+                        />
+                      </MDBDropdownToggle>
+                      <MDBDropdownMenu className="dropdown-default">
+                        <Link to="/settings" className="dropdown-item">
+                          Settings
+                        </Link>
+                        <Link
+                          to=""
+                          onClick={this.props.logout}
+                          className="dropdown-item"
+                        >
+                          Sign Out
+                        </Link>
+                      </MDBDropdownMenu>
+                    </MDBDropdown>
+                  </MDBNavItem>
+                </>
+              ) : (
+                <>
+                  {location.pathname !== "/" && (
+                    <Link
+                      to={{
+                        pathname: "/",
+                        state: {
+                          actionCard: 1,
+                        },
+                      }}
+                    >
+                      <MDBBtn color="dark" outline size="sm">
+                        Sign In
+                      </MDBBtn>
+                    </Link>
+                  )}
+                </>
+              )}
             </MDBNavbarNav>
           </MDBCollapse>
         </MDBContainer>
@@ -94,6 +141,18 @@ class Navbar extends React.Component<Props> {
     );
   }
 }
+//#endregion
+
+//#region > Redux Mapping
+const mapStateToProps = (state: any) => ({
+  loggedUser: { ...state.user.user },
+});
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    logout: () => dispatch(logoutAction()),
+  };
+};
 //#endregion
 
 //#region > Exports
@@ -105,7 +164,7 @@ class Navbar extends React.Component<Props> {
  * Got access to the history object’s properties and the closest
  * <Route>'s match.
  */
-export default withRouter(Navbar);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
 //#endregion
 
 /**
