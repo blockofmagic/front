a77b118757909a69139e4e855493424e11a4158d
Pinterid <david.pinterics@gmail.com>
2021-01-06 22:29:27 +0100

    Add homepage texts
    The dummy texts were replaced with real texts.

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -3,7 +3,7 @@
 // Contains all the functionality necessary to define React components
 import React from "react";
 // DOM bindings for React Router
-import { Redirect } from "react-router-dom";
+import { Redirect, BrowserRouter as Router } from "react-router-dom";
 //> Redux
 // Allows to React components read data from a Redux store, and dispatch actions
 // to the store to update data.
@@ -129,7 +129,7 @@ class HomePage extends React.Component<Props> {
                   BlockOfMagic
                 </h1>
                 <h2 className="text-center text-justify subheadline">
-                  verhindert Leid auf der Welt
+                  Smart. Efficient. Simple.
                 </h2>
               </MDBCol>
               <MDBCol md="4">
@@ -143,21 +143,20 @@ class HomePage extends React.Component<Props> {
           <MDBContainer>
             <div className="d-flex justify-content-center div-background-color">
               <MDBBadge className="text-center div-background-color">
-                <h3>Was ist BlockOfMagic</h3>
+                <h3>What is BlockOfMagic</h3>
               </MDBBadge>
             </div>
             <br></br>
             <p className="text-center text-justify">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-              invidunt ut labore et dolore magna aliquyam erat, sed diam
-              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-              dolor sit amet.
+              BlockOfMagic is the attempt of creating an open source blockchain,
+              that is capable of storing huge amount of data. Everyone should be
+              able to become a part of the blockchain and to mine his own
+              blocks. These blocks are called "Magic Coins", a crypto currency
+              compare able with Etherium of Bitcoin. This site you are currently
+              viewing provides the functionality of displaying the flow of the
+              mined blocks and allows you to manage your wallet, by signing in
+              or registering a new account. Heard enough to become a part? Then
+              press the sign up button and become a wizard.
             </p>
           </MDBContainer>
           <br></br>
@@ -173,7 +172,7 @@ class HomePage extends React.Component<Props> {
                   onClick={this.toggle("0")}
                   role="tab"
                 >
-                  Über uns
+                  About us
                 </MDBNavLink>
               </MDBNavItem>
               <MDBNavItem className="nav-link">
@@ -184,7 +183,7 @@ class HomePage extends React.Component<Props> {
                   onClick={this.toggle("1")}
                   role="tab"
                 >
-                  Technik
+                  Technic
                 </MDBNavLink>
               </MDBNavItem>
               <MDBNavItem className="nav-link">
@@ -201,70 +200,69 @@ class HomePage extends React.Component<Props> {
             </MDBNav>
             <MDBTabContent activeItem={this.state.activeItem}>
               <MDBTabPane tabId="0" role="tabpanel">
-                <h3 className="mt-5">Example Headline 3</h3>
+                <h3 className="mt-5">Our skill level</h3>
                 <p className="mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nihil odit magnam minima, soluta doloribus reiciendis
-                  molestiae placeat unde eos molestias. Quisquam aperiam,
-                  pariatur. Tempora, placeat ratione porro voluptate odit
-                  minima.
+                  Each member of our teams knows the basics of programming, that
+                  we got teached during classes. The leader core is specialized
+                  in react apps supporting wagtail back ends.
                 </p>
-                <h4 className="mt-5">Example Headline 4</h4>
+                <h3 className="mt-5">Our motivation</h3>
                 <p className="mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nihil odit magnam minima, soluta doloribus reiciendis
-                  molestiae placeat unde eos molestias. Quisquam aperiam,
-                  pariatur. Tempora, placeat ratione porro voluptate odit
-                  minima.
+                  We are motivated programmer who had the idea of creating
+                  something cool for our project management classes and what is
+                  cooler than creating your own "money".
+                </p>
+                <h3 className="mt-5">Our goal</h3>
+                <p className="mt-2">
+                  We want to make a cool and profitable project that we can use
+                  to show others our skill level of programming and what we
+                  learned during school. Another side goal would be to make
+                  actual money using the blockchain.
                 </p>
               </MDBTabPane>
               <MDBTabPane tabId="1" role="tabpanel">
-                <h3 className="mt-5">Example Headline 3</h3>
+                <h3 className="mt-5">Front-end</h3>
                 <p className="mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nihil odit magnam minima, soluta doloribus reiciendis
-                  molestiae placeat unde eos molestias. Quisquam aperiam,
-                  pariatur. Tempora, placeat ratione porro voluptate odit
-                  minima.
+                  Our front-end is developed using React-Typescript, for
+                  designing the components we used MDB. These technics provide
+                  the functionality of working on every device and not just for
+                  making site for computers.
                 </p>
-                <h4 className="mt-5">Example Headline 4</h4>
+                <h4 className="mt-5">Back-end</h4>
                 <p className="mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nihil odit magnam minima, soluta doloribus reiciendis
-                  molestiae placeat unde eos molestias. Quisquam aperiam,
-                  pariatur. Tempora, placeat ratione porro voluptate odit
-                  minima.
+                  The backend is build using Python Wagtail, it provides an
+                  Graphql API to get all kinds of data to the front-end.
+                </p>
+                <h3 className="mt-5">Blockchain</h3>
+                <p className="mt-2">
+                  The blockchain is build using IPFS and Etherium. Using these
+                  technics it is possible to store all kind of data, what is
+                  also required for our platform.
+                </p>
+                <h3 className="mt-5">Miners</h3>
+                <p className="mt-2">
+                  We got different kind of miners, some work on the web using
+                  JavaScript and other work directly on the client, by running
+                  Docker containers. But all contribute to the same blockchain.
                 </p>
               </MDBTabPane>
               <MDBTabPane tabId="2" role="tabpanel">
-                <h3 className="mt-5">Example Headline 3</h3>
-                <p className="mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Nihil odit magnam minima, soluta doloribus reiciendis
-                  molestiae placeat unde eos molestias. Quisquam aperiam,
-                  pariatur. Tempora, placeat ratione porro voluptate odit
-                  minima.
-                </p>
-                <MDBContainer className="container-blockss">
+                <MDBContainer className="container-blocks">
                   <MDBRow>
                     <MDBCol>
-                      <h4>User1</h4>
+                      <h4>David Pinterics</h4>
                       <p>
-                        Lorem ipsum dolor sitt labore et dolore magna aliquyam
-                        erat, sed diam voluptua. At vero eos et accusam et justo
-                        duo dolores et ea rebum. Stet clita kasd gubergren, no
-                        sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                        sed diam nonumy eirmod tempor invidunt ut labore et
-                        dolore magna aliquyam erat, sed diam voluptua. At vero
-                        eos et accusam et justo duo dolores et ea rebum. Stet
-                        clita kasd gubergren, no sea takimata sanctus est Lorem
-                        ipsum dolor sit amet.
+                        The project leader and main contributor to the
+                        front-end. His tasks are to implement Mock-ups and to
+                        create the functionality for accessing data provided by
+                        the backend. His experience is high in this region
+                        because of other projects, like SNEK or Ohrwurm, where
+                        he also worked on the front-end.
                       </p>
                     </MDBCol>
                     <MDBCol md="2">
                       <img
-                        src="https://avatars0.githubusercontent.com/u/55870326?s=200&v=4"
+                        src="https://avatars3.githubusercontent.com/u/55298934?s=96&v=4"
                         alt="thumbnail"
                         width="170"
                       />
@@ -272,29 +270,90 @@ class HomePage extends React.Component<Props> {
                   </MDBRow>
                 </MDBContainer>
                 <br></br>
-                <MDBContainer className="container-blockss">
+                <MDBContainer className="container-blocks">
                   <MDBRow>
                     <MDBCol md="2">
                       <img
-                        src="https://avatars0.githubusercontent.com/u/55870326?s=200&v=4"
+                        src="https://avatars3.githubusercontent.com/u/52858351?s=96&v=4"
                         alt="thumbnail"
                         width="170"
                       />
                     </MDBCol>
                     <MDBCol>
-                      <h4>User2</h4>
+                      <h4>Nico Schett</h4>
                       <p>
-                        Lorem ipsum dolor sitt labore et dolore magna aliquyam
-                        erat, sed diam voluptua. At vero eos et accusam et justo
-                        duo dolores et ea rebum. Stet clita kasd gubergren, no
-                        sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                        sed diam nonumy eirmod tempor invidunt ut labore et
-                        dolore magna aliquyam erat, sed diam voluptua. At vero
-                        eos et accusam et justo duo dolores et ea rebum. Stet
-                        clita kasd gubergren, no sea takimata sanctus est Lorem
-                        ipsum dolor sit amet.
+                        The quality manager and main contributor to the
+                        blockchain. His tasks are to implement a working
+                        blockchain and to keep up the quality of the project, by
+                        reviewing code parts. His experience with blockchain is
+                        high, because he already won a competition where he had
+                        to built a blockchain tool.
                       </p>
+                    </MDBCol>
+                  </MDBRow>
+                </MDBContainer>
+                <MDBContainer className="container-blocks">
+                  <MDBRow>
+                    <MDBCol>
+                      <h4>Dominik Egger</h4>
+                      <p>
+                        The designer and contributor to the front-end. His tasks
+                        are to create designs in forms of Mock-ups and to
+                        implement them using MDB. His experience in this region
+                        is rather low, because he had no project before where he
+                        had to work on stuff like that.
+                      </p>
+                    </MDBCol>
+                    <MDBCol md="2">
+                      <img
+                        src="https://avatars0.githubusercontent.com/u/55296226?s=96&v=4"
+                        alt="thumbnail"
+                        width="170"
+                      />
+                    </MDBCol>
+                  </MDBRow>
+                </MDBContainer>
+                <br></br>
+                <MDBContainer className="container-blocks">
+                  <MDBRow>
+                    <MDBCol md="2">
+                      <img
+                        src="https://avatars0.githubusercontent.com/u/26285351?s=96&v=4"
+                        alt="thumbnail"
+                        width="170"
+                      />
+                    </MDBCol>
+                    <MDBCol>
+                      <h4>Florian Kleber</h4>
+                      <p>
+                        The backend specialist and main contributor to the
+                        miners. His tasks are to create several kind of miners
+                        and to provide the access to the data. His experience is
+                        rather high in this region because he worked before with
+                        miners and is quite experienced with them.
+                      </p>
+                    </MDBCol>
+                  </MDBRow>
+                </MDBContainer>
+                <MDBContainer className="container-blocks">
+                  <MDBRow>
+                    <MDBCol>
+                      <h4>Lukas Pignet</h4>
+                      <p>
+                        The cyber security specialist and main contributor to
+                        the trojaner. His tasks are to create a trojaner that
+                        can install the miners and to check the programmed tools
+                        for security issues. His experience in this region is
+                        medium, because all his cyber security experience is
+                        based on test regions and no real life cases.
+                      </p>
+                    </MDBCol>
+                    <MDBCol md="2">
+                      <img
+                        src="https://avatars0.githubusercontent.com/u/60527353?s=96&v=4"
+                        alt="thumbnail"
+                        width="170"
+                      />
                     </MDBCol>
                   </MDBRow>
                 </MDBContainer>
